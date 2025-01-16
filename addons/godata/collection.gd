58091e74934bf0ca89ed06e5bdef4d4b40e774aa
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-16 17:28:21 -0300

    :sparkles: feat #6: created the DoublyLinkedDeque implementation

--- addons/godata/collection.gd
+++ addons/godata/collection.gd
@@ -2,6 +2,16 @@
 class_name GDCollection
 extends GDIterable
 
+signal emptied()
+
+signal filled()
+
+signal added(element: Variant)
+
+signal updated(old_element: Variant, new_element: Variant)
+
+signal removed(element: Variant)
+
 func length() -> int: return 0
 
 func is_empty() -> bool: return false
@@ -10,16 +20,62 @@ func is_full() -> bool: return false
 
 func add(element: Variant) -> bool: return false
 
-func add_all(elements: GDIterable) -> bool: return false
+func get_element() -> Variant: return null
 
-func has(element: Variant) -> bool: return false
-
-func has_all(elements: GDIterable) -> bool: return false
+func update(element: Variant) -> bool: return false
 
 func remove() -> Variant: return null
-
-func clear() -> bool: return false
 
 func equals(other: GDCollection) -> bool: return false
 
 func as_array() -> Array: return []
+
+func add_all(elements: GDIterable) -> bool:
+	var i: GDIterator = elements.iterator()
+	
+	var added_all: bool = true
+	
+	while i.has_next():
+		added_all = added_all and add(i.next())
+	
+	return added_all
+
+func has(element: Variant) -> bool:
+	var found: bool = false
+	
+	var i: GDIterator = iterator()
+	
+	while i.has_next():
+		found = found or (i.next() == element)
+		
+		if found:
+			break
+	
+	return found
+
+func has_all(elements: GDIterable) -> bool:
+	var found: bool = true
+	
+	var i: GDIterator = elements.iterator()
+	
+	while i.has_next():
+		found = found and has(i.next())
+		
+		if not found:
+			break
+	
+	return found
+
+func clear() -> bool:
+	if is_empty():
+		return false
+	
+	var i: GDIterator = iterator()
+	
+	while i.has_next():
+		i.next()
+		i.remove()
+	
+	emptied.emit()
+	
+	return true
---
+++ addons/godata/linear/deque/doubly_linked_deque/doubly_linked_deque_iterator.gd
@@ -0,0 +1,76 @@
+
+class_name GDDoublyLinkedDequeIterator
+extends GDIterator
+
+var deque: GDDoublyLinkedDeque = null:
+	set = set_deque,
+	get = get_deque
+
+var pointer: GDDoublyLinkedNode = null:
+	set = set_pointer,
+	get = get_pointer
+
+var last: GDDoublyLinkedNode = null:
+	set = set_last,
+	get = get_last
+
+func set_deque(new_deque: GDDoublyLinkedDeque) -> void:
+	deque = new_deque
+
+func get_deque() -> GDDoublyLinkedDeque:
+	return deque
+
+func set_pointer(new_node: GDDoublyLinkedNode) -> void:
+	pointer = new_node
+
+func get_pointer() -> GDDoublyLinkedNode:
+	return pointer
+
+func set_last(new_node: GDDoublyLinkedNode) -> void:
+	last = new_node
+
+func get_last() -> GDDoublyLinkedNode:
+	return last
+
+static func create(deque: GDDoublyLinkedDeque) -> GDIterator:
+	var iterator := GDDoublyLinkedDequeIterator.new()
+	iterator.deque = deque
+	iterator.pointer = deque.front
+	
+	return iterator
+
+func has_next() -> bool:
+	return pointer != null
+
+func next() -> Variant:
+	var result: Variant = pointer.value
+	
+	last = pointer
+	pointer = pointer.next
+	
+	return result
+
+func remove() -> void:
+	var prelast: GDDoublyLinkedNode = last.previous
+	
+	deque.removed.emit(last.value)
+	
+	if last == deque.front:
+		deque.removed_front.emit(last.value)
+	if last == deque.back:
+		deque.removed_back.emit(last.value)
+	
+	if prelast != null:
+		prelast.next = pointer
+	else:
+		deque.front = pointer
+	
+	last.next = null
+	last.previous = null
+	
+	if deque.is_empty():
+		deque.back = null
+		
+		deque.emptied.emit()
+	else:
+		pointer.previous = prelast
