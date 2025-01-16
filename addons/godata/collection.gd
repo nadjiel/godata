3b98d808f0e8965cec24de19215cd6ffd297900c
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-15 22:40:23 -0300

    :recycle: refac: changed remove signature in Collection

--- addons/godata/collection.gd
+++ addons/godata/collection.gd
@@ -16,7 +16,7 @@ func has(element: Variant) -> bool: return false
 
 func has_all(elements: GDIterable) -> bool: return false
 
-func remove() -> bool: return false
+func remove() -> Variant: return null
 
 func clear() -> bool: return false
 
