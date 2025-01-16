9a657a39cd3a3de520d6280806c81198e33a40af
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-16 12:03:37 -0300

    :sparkles: code: added accessors methods for LinkedNode

--- addons/godata/linked_node.gd
+++ addons/godata/linked_node.gd
@@ -2,9 +2,25 @@
 class_name GDLinkedNode
 extends Resource
 
-var value: Variant
+var value: Variant:
+	set = set_value,
+	get = get_value
 
-var next: GDLinkedNode
+var next: GDLinkedNode:
+	set = set_next,
+	get = get_next
+
+func set_value(new_value: Variant) -> void:
+	value = new_value
+
+func get_value() -> Variant:
+	return value
+
+func set_next(new_node: GDLinkedNode) -> void:
+	next = new_node
+
+func get_next() -> GDLinkedNode:
+	return next
 
 static func create(value: Variant) -> GDLinkedNode:
 	var node := GDLinkedNode.new()
