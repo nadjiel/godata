58572a406b5081b352f079bbd625382f1a4daa97
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-15 22:44:48 -0300

    :sparkles: implemented LinkedQueue and its Iterator

---
+++ addons/godata/linear/queue/linked_queue/linked_queue.gd
@@ -0,0 +1,86 @@
+
+class_name GDLinkedQueue
+extends GDQueue
+
+var back: GDLinkedNode = null
+
+var front: GDLinkedNode = null
+
+func iterator() -> GDIterator:
+	return GDLinkedQueueIterator.create(self)
+
+func length() -> int:
+	var counter: int = 0
+	
+	var i: GDIterator = iterator()
+	
+	while i.has_next():
+		counter += 1
+		
+		i.next()
+	
+	return counter
+
+func is_empty() -> bool:
+	return front == null
+
+func is_full() -> bool:
+	return false
+
+func add(element: Variant) -> bool:
+	var new_node := GDLinkedNode.create(element)
+	
+	back.next = new_node
+	back = new_node
+	
+	return true
+
+func add_all(elements: GDIterable) -> bool:
+	var i: GDIterator = elements.iterator()
+	
+	var added_all: bool = true
+	
+	while i.has_next():
+		added_all = added_all and add(i.next())
+	
+	return added_all
+
+func has(element: Variant) -> bool:
+	var found: bool = false
+	
+	var i: GDIterator = iterator()
+	
+	while i.has_next():
+		found = found or (i.next() == element)
+	
+	return found
+
+func has_all(elements: GDIterable) -> bool:
+	var found: bool = true
+	
+	var i: GDIterator = elements.iterator()
+	
+	while i.has_next():
+		found = found and has(i.next())
+	
+	return found
+
+func remove() -> Variant:
+	var old_node: GDLinkedNode = front
+	
+	front = front.next
+	old_node.next = null
+	
+	return old_node.value
+
+func clear() -> bool:
+	if is_empty():
+		return false
+	
+	var i: GDIterator = iterator()
+	
+	while i.has_next():
+		i.next()
+		i.remove()
+	
+	return true
---
+++ addons/godata/linear/queue/linked_queue/linked_queue_iterator.gd
@@ -0,0 +1,38 @@
+
+class_name GDLinkedQueueIterator
+extends GDIterator
+
+var queue: GDLinkedQueue = null
+
+var pointer: GDLinkedNode = null
+
+var last: GDLinkedNode = null
+
+var prelast: GDLinkedNode = null
+
+static func create(queue: GDLinkedQueue) -> GDIterator:
+	var iterator := GDLinkedQueueIterator.new()
+	iterator.queue = queue
+	iterator.pointer = queue.front
+	
+	return iterator
+
+func has_next() -> bool:
+	return pointer != null
+
+func next() -> Variant:
+	var result: Variant = pointer.value
+	
+	prelast = last
+	last = pointer
+	pointer = pointer.next
+	
+	return result
+
+func remove() -> void:
+	if prelast != null:
+		prelast.next = last.next
+	
+	last.next = null
+	last.free()
+	queue.front = pointer
