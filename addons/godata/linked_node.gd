96c32cb4dc0498d7ef2dfab14f7682c7559cad65
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-15 22:38:50 -0300

    :sparkles: code: created shortcut for creation

--- addons/godata/linked_node.gd
+++ addons/godata/linked_node.gd
@@ -5,3 +5,9 @@ extends Resource
 var value: Variant
 
 var next: GDLinkedNode
+
+static func create(value: Variant) -> GDLinkedNode:
+	var node := GDLinkedNode.new()
+	node.value = value
+	
+	return node
