3817977663866901623371b49829d2e9994b47e0
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-16 12:08:53 -0300

    :sparkles: code: added accessor methods for LinkedQueueIterator

--- addons/godata/linear/queue/linked_queue/linked_queue_iterator.gd
+++ addons/godata/linear/queue/linked_queue/linked_queue_iterator.gd
@@ -2,13 +2,45 @@
 class_name GDLinkedQueueIterator
 extends GDIterator
 
-var queue: GDLinkedQueue = null
+var queue: GDLinkedQueue = null:
+	set = set_queue,
+	get = get_queue
 
-var pointer: GDLinkedNode = null
+var pointer: GDLinkedNode = null:
+	set = set_pointer,
+	get = get_pointer
 
-var last: GDLinkedNode = null
+var last: GDLinkedNode = null:
+	set = set_last,
+	get = get_last
 
-var prelast: GDLinkedNode = null
+var prelast: GDLinkedNode = null:
+	set = set_prelast,
+	get = get_prelast
+
+func set_queue(new_queue: GDLinkedQueue) -> void:
+	queue = new_queue
+
+func get_queue() -> GDLinkedQueue:
+	return queue
+
+func set_pointer(new_node: GDLinkedNode) -> void:
+	pointer = new_node
+
+func get_pointer() -> GDLinkedNode:
+	return pointer
+
+func set_last(new_node: GDLinkedNode) -> void:
+	last = new_node
+
+func get_last() -> GDLinkedNode:
+	return last
+
+func set_prelast(new_node: GDLinkedNode) -> void:
+	last = new_node
+
+func get_prelast() -> GDLinkedNode:
+	return last
 
 static func create(queue: GDLinkedQueue) -> GDIterator:
 	var iterator := GDLinkedQueueIterator.new()
