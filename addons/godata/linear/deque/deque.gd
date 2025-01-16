1d57d5d275a8a531bb9771c92538cf5fb563ba02
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-16 15:00:23 -0300

    :recycle: refac: changed Deque to inherit from Linear

--- addons/godata/linear/deque/deque.gd
+++ addons/godata/linear/deque/deque.gd
@@ -1,25 +1,19 @@
 
 class_name GDDeque
-extends GDCollection
+extends GDLinear
 
-signal inserted(index: int, value: Variant)
-signal removed(index: int, value: Variant)
-signal updated(index: int, old_value: Variant, new_value: Variant)
+func add_front(element: Variant) -> void: pass
 
-func insert_first(element: Variant) -> void: pass
+func add_back(element: Variant) -> void: pass
 
-func insert_last(element: Variant) -> void: pass
+func get_front() -> Variant: return null
 
-func get_first() -> Variant: return null
+func get_back() -> Variant: return null
 
-func get_last() -> Variant: return null
+func update_front(element: Variant) -> bool: return false
 
-func get_element(id: int) -> Variant: return null
+func update_back(element: Variant) -> bool: return false
 
-func update_first(element: Variant) -> bool: return false
+func remove_front() -> Variant: return null
 
-func update_last(element: Variant) -> bool: return false
-
-func remove_first() -> Variant: return null
-
-func remove_last() -> Variant: return null
+func remove_back() -> Variant: return null
