8d9d2f0c619f6f82b6b178d41ef89214852e9a8d
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-18 16:11:52 -0300

    :sparkles: implemented the DictSet class

---
+++ addons/godata/linear/set/dict_set/dict_set.gd
@@ -0,0 +1,186 @@
+
+class_name GDDictSet
+extends GDSet
+
+var elements: Dictionary = {}
+
+static func from_array(array: Array) -> GDLinear:
+	var set: GDSet = GDDictSet.new()
+
+	for element: Variant in array:
+		set.add(element)
+	
+	return set
+
+func iterator() -> GDIterator:
+	return GDDictSetIterator.create(elements)
+
+func length() -> int:
+	return elements.size()
+
+func is_empty() -> bool:
+	return elements.is_empty()
+
+func add(element: Variant) -> bool:
+	if elements.has(element):
+		return false
+
+	set_element(element)
+	
+	return true
+
+func get_element() -> Variant:
+	return elements.keys().pick_random()
+
+func update(element: Variant) -> bool:
+	var had_element: bool = elements.has(element)
+	
+	set_element(element)
+
+	if not had_element:
+		return false
+
+	return true
+
+func remove() -> Variant:
+	var random_element: Variant = elements.keys().pick_random()
+
+	remove_element(random_element)
+
+	return random_element
+
+func equals(other: GDCollection) -> bool:
+	if not other is GDSet:
+		return false
+	
+	var is_superset: bool = true
+	var is_subset: bool = true
+
+	var this_i: GDIterator = iterator()
+	var other_i: GDIterator = other.iterator()
+
+	while this_i.has_next():
+		if not other_i.has_next():
+			return false
+		
+		is_superset = is_superset and other.has(this_i.next())
+
+		if not is_superset:
+			return false
+		
+		is_subset = is_subset and self.has(other_i.next())
+
+		if not is_subset:
+			return false
+	
+	if other_i.has_next():
+		return false
+
+	return true
+
+func has(element: Variant) -> bool:
+	return elements.has(element)
+
+func clear() -> bool:
+	if is_empty():
+		return false
+	
+	elements.clear()
+
+	return true
+
+func set_element(element: Variant) -> void:
+	elements[element] = null
+
+func remove_element(element: Variant) -> bool:
+	return elements.erase(element)
+
+func is_subset(other: GDSet) -> bool:
+	return other.has_all(self)
+
+func is_superset(other: GDSet) -> bool:
+	return has_all(other)
+
+func is_disjoint(other: GDSet) -> bool:
+	var found: bool = false
+
+	var i: GDIterator = iterator()
+
+	while i.has_next():
+		found = found and other.has(i.next())
+
+		if found:
+			return false
+
+	return true
+
+func union(other: GDSet) -> GDSet:
+	var result: GDSet = GDDictSet.new()
+
+	var this_i: GDIterator = iterator()
+	var other_i: GDIterator = other.iterator()
+
+	while this_i.has_next():
+		result.set_element(this_i.next())
+
+		if other_i.has_next():
+			result.set_element(other_i.next())
+
+	while other_i.has_next():
+		result.set_element(other_i.next())
+
+	return result
+
+func intersection(other: GDSet) -> GDSet:
+	var result: GDSet = GDDictSet.new()
+
+	var this_i: GDIterator = iterator()
+
+	while this_i.has_next():
+		var next_element: Variant = this_i.next()
+
+		if other.has(next_element):
+			result.set_element(next_element)
+
+	return result
+
+func difference(other: GDSet) -> GDSet:
+	var result: GDSet = GDDictSet.new()
+
+	var this_i: GDIterator = iterator()
+
+	while this_i.has_next():
+		var next_element: Variant = this_i.next()
+
+		if not other.has(next_element):
+			result.set_element(next_element)
+
+	return result
+
+func symetric_difference(other: GDSet) -> GDSet:
+	var result: GDSet = GDDictSet.new()
+
+	var this_i: GDIterator = iterator()
+	var other_i: GDIterator = other.iterator()
+
+	while this_i.has_next():
+		var this_element: Variant = this_i.next()
+
+		if not other.has(this_element):
+			result.set_element(this_element)
+
+		if not other_i.has_next():
+			continue
+		
+		var other_element: Variant = other_i.next()
+
+		if not self.has(other_element):
+			result.set_element(other_element)
+
+	while other_i.has_next():
+		var other_element: Variant = other_i.next()
+
+		if not self.has(other_element):
+			result.set_element(other_element)
+
+	return result
