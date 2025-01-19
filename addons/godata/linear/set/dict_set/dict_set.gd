683cdb080d46a4e6a53f2778c2b8d38fe6985114
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-19 12:35:52 -0300

    :sparkles: feat: made DictSet emit signals properly

--- addons/godata/linear/set/dict_set/dict_set.gd
+++ addons/godata/linear/set/dict_set/dict_set.gd
@@ -6,7 +6,7 @@ var elements: Dictionary = {}
 
 static func from_array(array: Array) -> GDLinear:
 	var set: GDSet = GDDictSet.new()
-
+	
 	for element: Variant in array:
 		set.add(element)
 	
@@ -24,7 +24,7 @@ func is_empty() -> bool:
 func add(element: Variant) -> bool:
 	if elements.has(element):
 		return false
-
+	
 	set_element(element)
 	
 	return true
@@ -36,17 +36,17 @@ func update(element: Variant) -> bool:
 	var had_element: bool = elements.has(element)
 	
 	set_element(element)
-
+	
 	if not had_element:
 		return false
-
+	
 	return true
 
 func remove() -> Variant:
 	var random_element: Variant = elements.keys().pick_random()
-
+	
 	remove_element(random_element)
-
+	
 	return random_element
 
 func equals(other: GDCollection) -> bool:
@@ -55,27 +55,27 @@ func equals(other: GDCollection) -> bool:
 	
 	var is_superset: bool = true
 	var is_subset: bool = true
-
+	
 	var this_i: GDIterator = iterator()
 	var other_i: GDIterator = other.iterator()
-
+	
 	while this_i.has_next():
 		if not other_i.has_next():
 			return false
 		
 		is_superset = is_superset and other.has(this_i.next())
-
+		
 		if not is_superset:
 			return false
 		
 		is_subset = is_subset and self.has(other_i.next())
-
+		
 		if not is_subset:
 			return false
 	
 	if other_i.has_next():
 		return false
-
+	
 	return true
 
 func copy() -> GDCollection:
@@ -91,14 +91,31 @@ func clear() -> bool:
 		return false
 	
 	elements.clear()
-
+	
+	emptied.emit()
+	
 	return true
 
 func set_element(element: Variant) -> void:
+	var had_element: bool = elements.has(element)
+	
 	elements[element] = null
+	
+	if not had_element:
+		added.emit(element)
+		updated.emit(null, element)
+	else:
+		updated.emit(element, element)
 
 func remove_element(element: Variant) -> bool:
-	return elements.erase(element)
+	var was_removed: bool = elements.erase(element)
+	
+	if was_removed:
+		removed.emit(element)
+	if is_empty():
+		emptied.emit()
+	
+	return was_removed
 
 func is_subset(other: GDSet) -> bool:
 	return other.has_all(self)
@@ -121,71 +138,71 @@ func is_disjoint(other: GDSet) -> bool:
 
 func union(other: GDSet) -> GDSet:
 	var result: GDSet = GDDictSet.new()
-
+	
 	var this_i: GDIterator = iterator()
 	var other_i: GDIterator = other.iterator()
-
+	
 	while this_i.has_next():
 		result.set_element(this_i.next())
-
+		
 		if other_i.has_next():
 			result.set_element(other_i.next())
-
+	
 	while other_i.has_next():
 		result.set_element(other_i.next())
-
+	
 	return result
 
 func intersection(other: GDSet) -> GDSet:
 	var result: GDSet = GDDictSet.new()
-
+	
 	var this_i: GDIterator = iterator()
-
+	
 	while this_i.has_next():
 		var next_element: Variant = this_i.next()
-
+		
 		if other.has(next_element):
 			result.set_element(next_element)
-
+	
 	return result
 
 func difference(other: GDSet) -> GDSet:
 	var result: GDSet = GDDictSet.new()
-
+	
 	var this_i: GDIterator = iterator()
-
+	
 	while this_i.has_next():
 		var next_element: Variant = this_i.next()
-
+		
 		if not other.has(next_element):
 			result.set_element(next_element)
-
+	
 	return result
 
 func symetric_difference(other: GDSet) -> GDSet:
 	var result: GDSet = GDDictSet.new()
-
+	
 	var this_i: GDIterator = iterator()
 	var other_i: GDIterator = other.iterator()
-
+	
 	while this_i.has_next():
 		var this_element: Variant = this_i.next()
-
+		
 		if not other.has(this_element):
 			result.set_element(this_element)
-
+		
 		if not other_i.has_next():
 			continue
 		
 		var other_element: Variant = other_i.next()
-
+		
 		if not self.has(other_element):
 			result.set_element(other_element)
-
+	
 	while other_i.has_next():
 		var other_element: Variant = other_i.next()
-
+		
 		if not self.has(other_element):
 			result.set_element(other_element)
-
+	
 	return result
