938a7268edef968935a524283af9ae996bf58705
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-19 12:16:19 -0300

    :sparkles: feat: created iterable versions of Set's operations

--- addons/godata/linear/set/set.gd
+++ addons/godata/linear/set/set.gd
@@ -19,3 +19,55 @@ func intersection(other: GDSet) -> GDSet: return null
 func difference(other: GDSet) -> GDSet: return null
 
 func symetric_difference(other: GDSet) -> GDSet: return null
+
+func union_all(others: GDIterable) -> GDSet:
+	var result: GDSet = self
+	
+	var i: GDIterator = others.iterator()
+	
+	while i.has_next():
+		result = result.union(i.next())
+	
+	if result == self:
+		result = self.copy()
+	
+	return result
+
+func intersection_all(others: GDIterable) -> GDSet:
+	var result: GDSet = self
+	
+	var i: GDIterator = others.iterator()
+	
+	while i.has_next():
+		result = result.intersection(i.next())
+	
+	if result == self:
+		result = self.copy()
+	
+	return result
+
+func difference_all(others: GDIterable) -> GDSet:
+	var result: GDSet = self
+	
+	var i: GDIterator = others.iterator()
+	
+	while i.has_next():
+		result = result.difference(i.next())
+	
+	if result == self:
+		result = self.copy()
+	
+	return result
+
+func symetric_difference_all(others: GDIterable) -> GDSet:
+	var result: GDSet = self
+	
+	var i: GDIterator = others.iterator()
+	
+	while i.has_next():
+		result = result.symetric_difference(i.next())
+	
+	if result == self:
+		result = self.copy()
+	
+	return result
