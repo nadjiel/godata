cfc8de75a907c22dcadd7fed86a905b368fc5139
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2025-01-19 12:15:34 -0300

    :sparkles: code: added a copy method signature in Collection

--- addons/godata/collection.gd
+++ addons/godata/collection.gd
@@ -120,6 +120,8 @@ func remove() -> Variant: return null
 ## taking their elements in account, and not their memory addresses.
 func equals(other: GDCollection) -> bool: return false
 
+func copy() -> GDCollection: return null
+
 ## The [method as_array] method should be overwritten for providing a way
 ## of recovering a version of this [GDCollection] converted into an [Array].
 func as_array() -> Array: return []
